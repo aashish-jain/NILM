ddf8d26c029aca00dec350faa44aad081db4be41
Aashish <s.aashishkumarjain@gmail.com>
2016-02-27 17:16:23 +0530

    model 3.0

--- Charts.py
+++ Charts.py
@@ -3,7 +3,7 @@ from  openpyxl import (Workbook,load_workbook)
 
 def extract_Ipeaks(device,c):
     wb = load_workbook('/home/aashish/DI/data/'+device+'.xlsx')                 #create instance of load_workbook
-    work_sheet=wb['Sheet']                                                      #select the required worksheet
+    work_sheet=wb['Sheet1']                                                      #select the required worksheet
     c=randrange(65,85) if c==0 else c+64                                        #if no trial specified choose any random trail
     data=[]                                                                     #list to store Ipeak values
     print '*'*39,chr(c),'*'*40
@@ -13,21 +13,21 @@ def extract_Ipeaks(device,c):
 
 def select_Ipeaks(data):
     i=0
-    while(data[i+1]-data[i]<5):
+    while(data[i+1]-data[i]<4 and i<(len(data)-2)):
         i+=1
-        print i,' ',data[i],' ',data[i+1]
     i-=2
     return data[i:]
 
-devices=['Bulb','Fan','Heater','Cooler','Soldering Iron']
+devices=['Bulb_Cooler','Bulb_Fan']
 #creating a new excel sheet
-wb= Workbook()
+wb=Workbook()
 ws=wb.active
 
 for device in devices:
     ws['A1']='Number'
+    raw_input("press any key")
     print device
-    for x in range(2,52):
+    for x in range(2,62):
         ws['A'+str(x)]=str(x-1)
     for i in range(1,21):
         data=select_Ipeaks(extract_Ipeaks(device,i))
@@ -37,10 +37,25 @@ for device in devices:
         print '_'*80
         ws[chr(i+65)+'1']='trial'+str(i)
         k=2
-        for j in data:
-            ws[chr(i+65)+str(k)]=j-data[0]
+        temp=[]
+        for j in range(0,len(data)):
+            print i,' ',j,' ',k
+            ws[chr(i+65)+str(k)]=data[j]-data[0]
             k+=1
-            if(k>51):
-                break;
-        wb.save('/home/aashish/Desktop/data/charts/'+device+'_50.xlsx')
+            j+=1
+            if(k>31):
+                temp=data[j:]
+                break
+        # raw_input("Enter anything")
+        temp=select_Ipeaks(temp)
+        # print temp
+        # raw_input("Enter anything")
+        for j in range(0,len(temp)):
+            print i,' ',j,' ',k
+            ws[chr(i+65)+str(k)]=temp[j]-data[0]
+            k+=1
+            j+=1
+            if(k>61):
+                break
+        wb.save('/home/aashish/DI/charts/'+device+'_50.xlsx')
 print "WorkBooks created"
--- DI.py
+++ DI.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-'''    Non Intrusive device identification - characteristics extraction and identification
-       Smart Devices group-Solarillion Foundation
+'''
+        Non Intrusive device identification - characteristics extraction and identification
+        Smart Devices group-Solarillion Foundation
 '''
 
-from openpyxl import load_workbook
-from random import randrange
-from statistics import mean
-from math import sqrt
 '''
     PIP-> pyhton library manager
     to install library commmand is:
@@ -25,121 +22,119 @@ from math import sqrt
     runtime-> device identification
 '''
 
+from openpyxl import load_workbook
+from random import randrange
+from statistics import mean
+from math import sqrt
 
 #constant(s) set after empirically observing the data
-minimum_jump_magnitude=5
+minimum_jump_magnitude=3
 t=[]
-devices=['Bulb','Fan','Heater','Cooler','Soldering Iron']
+devices=['D1','D2','D3','D4','D5']
 
 class C1:
     jump_magnitude=0
-    jump_instance=0
+    first_maxima=0
 
 class C2:
-    first_maxima=0
-    first_maxima_index=0
+    avg_transient_ipeaks=0
+    settling_time=0
 
 class C3:
-    settling_time=0
-    avg_transient_ipeaks=0
-    avg_steadystate_ipeaks=0.0
-
+    avg_steadystate=0
 
 class template_library(C1,C2,C3):
     device=''
     def print_val(self):
         print '_'*80
         print self.device
-        print "jump_magnitude={0} jump_instance={1} ".format(self.jump_magnitude,self.jump_instance)
-        print "first_maxima={0} first_maxima_index={1} ".format(self.first_maxima,self.first_maxima_index)
-        print "avg_steadystate_ipeaks={0} settling_time={1} avg_transient_ipeaks={2}".format(self.avg_steadystate_ipeaks,self.settling_time,self.avg_transient_ipeaks)
+        print "jump_magnitude={0} first_maxima={1} ".format(self.jump_magnitude,self.first_maxima)
+        print " avg_transient_ipeaks={0} settling_time={0}ms".format(self.avg_transient_ipeaks,self.settling_time*20)
+        print " avg_steadystate={0}".format(self.avg_steadystate)
         print '_'*80
 
 def extract_characteristics(data,device=""):                                    #for extracting characteristics
+    ptr=data
     t=template_library()                                                        #for storing the characteristics
     t.device=device
+    previous_steadystate=data[0]                                                #Assuming first value of data to be SS value
     i=1                                                                         #setting index count of data to be 1
     #c1
-    previous_steadystate=data[0]                                                #Assuming first value of data to be SS value
     while(data[i]-previous_steadystate<minimum_jump_magnitude):                 #traverse the array till a jump is detected
         i+=1
-    reference=i
-    i+=1                                                                        #considering the second instantaneous Ipeak value
-    t.jump_instance=2
+    data=data[(i-1):]                                                           #truncating the Ipeak Data
+    i=2                                                                         #considering the second Ipeak value
     t.jump_magnitude=data[i]-previous_steadystate                               #storing jump_magnitude
     i+=1
     #c2
     while(data[i]<data[i+1]):                                                   #finding first maxima
         i+=1
     t.first_maxima=data[i]-previous_steadystate                                 #storing first maxima magnitude
-    t.first_maxima_index=i                                                      #storing first maxima index
     i+=1
-    #c3
+    #transient and steady state average, settling time
     temp=[]
-    i+=2
-    while(data[i]-data[i+5]>3):
-        temp.append(data[i]-data[i+1])
+    while( data[i] - data[i+1] > 1 ):                                           #transient average calculation
+        temp.append(data[i]-previous_steadystate)
         i+=1
-    t.settling_time=i-reference
+    t.settling_time=i
     if(len(temp)!=0):
         t.avg_transient_ipeaks=mean(temp)
-    j=i
-    while(i<len(data) and i-j<11):                                               #Ipeak average is calculated for 5 rises
-        t.avg_steadystate_ipeaks+=(data[i]-previous_steadystate);
+    del temp
+    data=data[i:]
+    i=0
+    while(i<len(data) and i<10):
+        t.avg_steadystate+=(data[i]-previous_steadystate);
         i+=1
-
-    t.avg_steadystate_ipeaks/=(i-j)
+    t.avg_steadystate/=i
+    del ptr
     return t
 
 #reads data from given excel sheet (name of excel file and sheet should be same)
 def read_excelsheet(device,c=0):
-    wb = load_workbook('/home/aashish/DI/data/'+device+'.xlsx')                 #create instance of load_workbook
-    work_sheet=wb['Sheet']                                                       #select the required worksheet
+    wb = load_workbook('/home/aashish/DI/data/1/'+device+'.xlsx')               #create instance of load_workbook
+    work_sheet=wb['Sheet1']                                                     #select the required worksheet
     c=randrange(65,85) if c==0 else c+64                                        #if no trial specified choose any random trail
     data=[]                                                                     #list to store Ipeak values
-    for i in range(2,202):                                                      #copy values from cells and store it in data list
+    for i in xrange(2,202):                                                      #copy values from cells and store it in data list
         data.append(work_sheet[chr(c)+str(i)].value)
     return extract_characteristics(data,device)                                 #return the extracted chracterisitcs from data
 
 def euclidean_distance_list(on_device):
     d=[]
-    for i in range(0,len(t)):
-        temp=(t[i].jump_magnitude - on_device.jump_magnitude)**2;
-        temp+=(t[i].first_maxima - on_device.first_maxima)**2
-        temp+=(t[i].avg_transient_ipeaks - on_device.avg_transient_ipeaks)**2
-        temp+=(t[i].settling_time - on_device.settling_time)**2
-        temp+=(t[i].avg_steadystate_ipeaks - on_device.avg_steadystate_ipeaks)**2
-        d.append(sqrt(abs(temp)))
+    for i in xrange(0,len(t)):
+        temp=[]
+        temp.append((t[i].jump_magnitude - on_device.jump_magnitude)**2)
+        temp.append((t[i].first_maxima - on_device.first_maxima)**2)
+        temp.append((t[i].avg_transient_ipeaks - on_device.avg_transient_ipeaks)**2)
+        temp.append((t[i].settling_time - on_device.settling_time)**2)
+        temp.append((t[i].avg_steadystate - on_device.avg_steadystate)**2)
+        # print i+1,' Temp= ',
+        # for i in xrange(0,len(temp)):
+        #     print (str(round(sqrt(temp[i]),2))).ljust(6),
+        # print
+        d.append(sqrt(abs(sum(temp))))
+        del temp
     return d
 
 def identify_device():
     for device in devices:
         print '_'*80
-        print 'Actual Device : '+device
-        for i in range(1,21):
+        print 'Actual Device : ',device,' '
+        for i in xrange(1,21):
             distance=euclidean_distance_list(read_excelsheet(device,i))
-            # for i in range(0,len(devices)):
+            # for i in xrange(0,len(devices)):
             #     print (str(round(distance[i],2))).ljust(6),
-            # print
             min_index=0
-            for i in range(0,len(distance)):
+            for i in xrange(1,len(distance)):
                 if(distance[min_index]>distance[i]):
                     min_index=i
-            try:
-                print devices[min_index]
-            except:
-                print len(distance),' ',min_index
-            # max_index=0
-            # for j in range(1,len(distance_list)):
-            #     if distance_list[max_index]<distance_list[j]:
-            #         max_index=j
-            # print device[max_index]
+            print devices[min_index]
 
 #Main code exectution starts here
-trial_number=randrange(1,21)
+trial_number=int(raw_input('Enter the trail number :'))
 print "trial_number ",trial_number
 for device in devices:
     t.append(read_excelsheet(device,trial_number))
-for i in range(0,len(t)):
-    t[i].print_val()
+# for i in xrange(0,len(t)):
+#     t[i].print_val()
 identify_device()
